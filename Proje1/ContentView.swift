3a4457c2e011c67684bb9b47da601e301bc0cddf
Ivan Terekhov <terekhovvan@yahoo.com>
2025-07-23 14:38:53 +0300

    Первый коммит

---
+++ Proje1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Proje1
+//
+//  Created by Иван Терехов on 23.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Proje1/Proje1App.swift
@@ -0,0 +1,17 @@
+//
+//  Proje1App.swift
+//  Proje1
+//
+//  Created by Иван Терехов on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Proje1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
